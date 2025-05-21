480dbaadd7c6470d75a54ee5bec5943e18e6d90e
keshkaQ <kirill.banar44@gmail.com>
2025-05-21 15:36:50 +0300

    Добавьте файлы проекта.

---
+++ WPF_/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ WPF_/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (String.IsNullOrWhiteSpace(x.Text) || (String.IsNullOrWhiteSpace(y.Text)))
+        {
+            MessageBox.Show("Поля не могут быть пустыми");
+            return;
+        }
+        if (!double.TryParse(x.Text, out double xNum) || !double.TryParse(y.Text, out double yNum))
+        {
+            MessageBox.Show("Введите корректные числа");
+            return;
+        }
+        result.Text = $"{xNum} + {yNum} = {xNum + yNum}";
+    }
+}
